dc0d09acd864d4fd0d5fe6b859b0d5a855d1f31c
BryanGrana <bryangranamartinez@gmail.com>
2024-04-12 21:36:11 +0200

    Mi amor la mejor tester uwu

--- JS/ej02/script.js
+++ JS/ej02/script.js
@@ -1,7 +1,15 @@
 let contenido = document.getElementById("contenido");
 contenido.textContent = "Hola mundo!";
 
+let indice = 0;
+
 setInterval(() => {
     let colores = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#00FFFF", "#FF00FF"];
-    contenido.style.backgroundColor = colores[Math.floor(Math.random() * colores.length)];
+    contenido.style.backgroundColor = colores[indice];
+
+    indice++;
+    if (indice === colores.length) {
+        indice = 0;
+    }
+
 }, 10);
--- JS/ej05/script.js
+++ JS/ej05/script.js
@@ -2,18 +2,21 @@ document.getElementById('btnStart').addEventListener('click', function () {
 
     let segundos = parseInt(document.getElementById('segundos').value);
 
-    let tiempoRestante = segundos;
+    if (isNaN(segundos) || segundos <= 0) {
 
-    let contador = setInterval(function () {
+    } else {
+        let tiempoRestante = segundos;
+        let contador = setInterval(function () {
 
-        tiempoRestante--;
-        document.getElementById('resultado').textContent = `Tiempo restante: ${tiempoRestante} segundos`;
-        
-        if (tiempoRestante === 0) {
-            clearInterval(contador);
-            document.getElementById('resultado').textContent = '¡La alarma está sonando!';
-        }
+            tiempoRestante--;
+            document.getElementById('resultado').textContent = `Tiempo restante: ${tiempoRestante} segundos`;
 
-    }, 1000);
+            if (tiempoRestante === 0) {
+                clearInterval(contador);
+                document.getElementById('resultado').textContent = '¡La alarma está sonando!';
+            }
+
+        }, 1000);
+    }
 
 });
